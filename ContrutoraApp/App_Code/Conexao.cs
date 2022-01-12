b37a959a9f0360f0bc01166e23b838e97f7705a7
wettooliveira <w_oliveira3@hotmail.com>
2022-01-11 21:27:17 -0300

    conexao

--- ContrutoraApp/App_Code/Conexao.cs
+++ ContrutoraApp/App_Code/Conexao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -12,8 +13,11 @@ namespace ContrutoraApp
         {
             get
             {
-              //  return @"Data Source=DESKTOP-0RKSTU6\\SQLEXPRESS;Initial Catalog=Sistema-Wellington; Integrated Security=True; Connect Timeout=30; User=DESKTOP-0RKSTU6\\Wellington;Password=";
-                return @"Data Source= BRSAVENDL101377; Initial Catalog= CONSTRUTORA; Integrated Security= True; Connect Timeout= 30; User= BFUSA\OliveiraWel; Password= ";
+                //  return @"Data Source=DESKTOP-0RKSTU6\\SQLEXPRESS;Initial Catalog=Sistema-Wellington; Integrated Security=True; Connect Timeout=30; User=DESKTOP-0RKSTU6\\Wellington;Password=";
+                // return @"Data Source= BRSAVENDL101377; Initial Catalog= CONSTRUTORA; Integrated Security= True; Connect Timeout= 30; User= BFUSA\OliveiraWel; Password= ";
+
+                    return ConfigurationManager.ConnectionStrings["construtora"].ConnectionString;
+                             
             }
         }
 
